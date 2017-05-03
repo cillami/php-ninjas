0c04d3e6b86ab53ca8c14506ff3af728e99a440f
Mohsin Rahman <mohsinrahmang@gmail.com>
2017-05-03 14:18:22 +0200

    add signin

--- partials/navbar.php
+++ partials/navbar.php
@@ -6,11 +6,7 @@
 
 	<div class="collapse navbar-collapse" id="navbarSupportedContent">
 		<ul class="navbar-nav ml-auto">
-			<form class="form-inline">
-				<input name="username" class="form-control mr-sm-2" type="text" placeholder="Username">
-				<input name="password" class="form-control mr-sm-2" type="password" placeholder="Password">
-				<button class="btn btn-success my-2 my-sm-0" type="submit">Sign </button>
-			</form>
+			<?php include "partials/signin-view.php";?>
 			<li class="nav-item">
 				<a class="nav-link" href="partials/form.php">FORM</a>
 			</li>
