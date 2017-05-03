ad1885a6f1e6d6f75e1210cae11487ec069d3a27
Eddie Jostell <eddie.jostell@gmail.com>
2017-05-03 15:51:29 +0200

    sorted som margin on index.php

--- partials/signup.php
+++ partials/signup.php
@@ -1,24 +1,24 @@
 <div class="col-md-6">
-        <section class=" con-signup">
+        <section class="margin-t con-signup">
           <form action="partials/register.php" method="POST">
             <div class="form-group">
               <label class="inputLabel" for="username">Username</label>
               <input name="username" type="text" class="form-control" id="username" placeholder="">
             </div>
             <div class="form-group">
-              <label for="password">Password</label>
+              <label class="inputLabel" for="password">Password</label>
               <input name="password" type="password" class="form-control" id="password" placeholder="">
             </div>
             <div class="form-group">
-              <label for="email">Email</label>
+              <label class="inputLabel" for="email">Email</label>
               <input name="email" type="email" class="form-control" id="email" placeholder="">
             </div>
             <div class="form-group">
-              <label for="firstname">Firstname</label>
+              <label class="inputLabel" for="firstname">Firstname</label>
               <input name="firstname" type="text" class="form-control" id="firstname" placeholder="">
             </div>
             <div class="form-group">
-              <label for="firstname">Lastname</label>
+              <label class="inputLabel" for="firstname">Lastname</label>
               <input name="lastname" type="text" class="form-control" id="firstname" placeholder="">
             </div>
             <button type="submit" class="btn btn-primary">Submit</button>
--- partials/welcometext.php
+++ partials/welcometext.php
@@ -1,6 +1,8 @@
 <div class="col-md-6">
-			<h1>HELLO!</h1>
-			<h1>WELCOME TO THE NINJABLOG PORTAL</h1>
-			<h1>PLEASE SIGN UP TO BE ABLE TO USE OUR SERVICE</h1>
-			<h1>ENJOY YOUR STAY!</h1>
-		</div>
+<section class="margin-t">
+		<h1>HELLO!</h1>
+		<h1>WELCOME TO THE NINJABLOG PORTAL</h1>
+		<h1>PLEASE SIGN UP TO BE ABLE TO USE OUR SERVICE</h1>
+		<h1>ENJOY YOUR STAY!</h1>
+	</section>
+</div>
