8e7908e9a242abd0016c10fd2191fa521df3dbc8
cillami <cillaminami@gmail.com>
2017-05-03 15:48:09 +0200

    hash login

--- partials/signin.php
+++ partials/signin.php
@@ -2,29 +2,41 @@
 session_start();
 include "error.php";
 include "database.php";
+
+
+
 if (isset($_POST["username"]) && isset($_POST["password"])) {
 
 	$username = $_POST["username"];
 	$password = $_POST["password"];
-	$statement = $pdo->prepare("SELECT * FROM user WHERE username = :username AND password = :password
-	");
-$statement->execute(
-	[":username" => $username,
-     ":password" => $password
-	]
-	);
-$data = $statement->fetch(PDO::FETCH_ASSOC);
-    
-    if ($data > 0){
 
-    	$_SESSION["username"]= $username ;
-    	$_SESSION["password"]= $password ;
-    	header("Location: /php-ninjas/partials/home.php");
-    }
+	// var_dump($_POST["password"]);
+	// $hash = password_hash($password, PASSWORD_DEFAULT);
 
+	$statement = $pdo->prepare("SELECT * FROM user WHERE username = :username
+		");
 
+	$statement->execute(
+		[":username" => $username
+		// ":password" => $password
+		]
+		);
 
-	
+	$data = $statement->fetch(PDO::FETCH_ASSOC);
+
+	if ($data > 0){
+
+		if(password_verify($password, $data[0]['password'])){
+
+			$_SESSION["username"]= $username ;
+			$_SESSION["password"]= $password ;
+			header("Location: /php-ninjas/partials/home.php");
+		}
+
+		else{
+			echo "Fel användarnamn eller password, försök igen!"; 
+			header("Location: ../index.php");
+		}
+	}
+
 }
-
-?>
