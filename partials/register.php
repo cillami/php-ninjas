5f7f6da2cbede31ae37fbe6ab25646b9f16f665e
cillami <cillaminami@gmail.com>
2017-05-08 10:37:31 +0200

    compare user in database with input

--- partials/register.php
+++ partials/register.php
@@ -14,52 +14,40 @@ class Register{
 
 	public function userInDb(){
 
-		$statement = $this->pdo->prepare("SELECT username FROM users 
+		$statement = $this->pdo->prepare("SELECT username FROM users WHERE username = :username
 			");
-		$statement->execute();
+		$statement->execute([":username" => $_POST['username']]);
 
-		$checkUser = $statement->fetchAll(PDO::FETCH_ASSOC);
-
-		// var_dump($checkUser[0]['username']);
-
-			foreach($checkUser as $user){
-			$userDb = $user['username'];
-		}
-		var_dump($user['username']);
+		return $statement->fetchAll(PDO::FETCH_ASSOC);
 
 	}
 
 	public function createUser(){
+		// var_dump($user);
 
-		$checkUsers = $this->userInDb();
-		var_dump($checkUsers);
+		if($this->userInDb() == true)
+		{
+			echo "Username already exists!";
+		}
+		else 
+		{
+			$hash = password_hash($_POST['password'], PASSWORD_DEFAULT); 
 
-
-
-
-	  	// $hej = $this->userInDb($this->checkUser); // ??
-		// var_dump($hej);
-
-		$hash = password_hash($_POST['password'], PASSWORD_DEFAULT); 
-
-		$statement = $this->pdo->prepare("
-			INSERT INTO users (username, password, email, firstname, lastname)
-			VALUES (:username, :password, :email, :firstname, :lastname)");
+			$statement = $this->pdo->prepare("
+				INSERT INTO users (username, password, email, firstname, lastname)
+				VALUES (:username, :password, :email, :firstname, :lastname)");
 
 //Execute statement, fetch data
-		$statement->execute([
-			":username" => $_POST['username'],
-			":password" => $hash,
-			":email" => $_POST['email'],
-			":firstname" => $_POST['firstname'],
-			":lastname" => $_POST['lastname']
-			]);
-
-
+			$statement->execute([
+				":username" => $_POST['username'],
+				":password" => $hash,
+				":email" => $_POST['email'],
+				":firstname" => $_POST['firstname'],
+				":lastname" => $_POST['lastname']
+				]);
 		header('Location: /php-ninjas');
+		}
+
 } //end method
 
 } //end class
-
-$userInDb = new Register($pdo);
-$userInDb->userInDb();
