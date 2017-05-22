b77cb4a08b6eb2987dbf5789e27330fb5f09a08c
cillami <cillaminami@gmail.com>
2017-05-22 10:37:29 +0200

    re-designed the front page some more

--- partials/welcometext.php
+++ partials/welcometext.php
@@ -2,8 +2,10 @@
 	<section class="margin-t">
 <!-- 		<h1 class="text-center">HELLO!</h1> -->
 		
-		<h1 class="text-center welcome">Join DigitizedLife today! </h1><br>
-		<h2 class="text-center white">Please sign up to be able to use our service</h2>
-		<br><h2 class="text-center white">Enjoy your stay!</h2>
+<!-- 				<p class="text-center welcome">Snapshot your life and share it with your friends!<br> -->
+		<p class="text-center welcome">Join DigitizedLife today! </p><br>
+
+		<h3 class="text-center white">Please sign up to be able to use our service. Enjoy your stay!</h3>
+<!-- 		<br><p class="text-center white welcome">Enjoy your stay!</p> -->
 		</section>
 	</div>
